b64564991282c7d2d754f43d6b6a2b111ffaa6ff
SwannBali <swannbld@gmail.com>
2025-09-19 14:07:14 +0200

    add auto creation of subnet for each zone

--- main.tf
+++ main.tf
@@ -4,3 +4,39 @@ provider "aws" {
   region = var.aws_region
 }
 
+
+# Step 1: Create VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-vpc"
+    Terraform = true
+  })
+}
+
+# Step 2: Create Subnets
+resource "aws_subnet" "public_subnet" {
+  # for_each = var.azs
+  for_each = var.azs
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 4, each.value)
+  availability_zone = "${var.aws_region}${each.key}"
+
+  tags = {
+    Name = "${var.vpc_name}-public-${var.aws_region}${each.key}"
+    Terraform = true
+  } 
+}
+
+resource "aws_subnet" "private_subnet" {
+  for_each = var.azs
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 4, 15 - each.value)
+  availability_zone = "${var.aws_region}${each.key}"
+
+  tags = {
+    Name = "${var.vpc_name}-private-${var.aws_region}${each.key}"
+    Terraform = true
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,29 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "vpc_cidr_block" {
+  type    = string
+  default = "172.20.0.0/16"
+}
+
+
+variable "vpc_name" {
+  type    = string
+  default = "insset-ccm"
+}
+
+variable "tags" {
+  type    = map(string)
+  description = "Tags to apply to the resources"
+  default = {}
+}
+
+variable "azs" {
+  type    = map(any)
+  description = "Availability Zones to deploy in vpc"
+  default = {
+    a = 1
+    b = 2
+  }
+}
